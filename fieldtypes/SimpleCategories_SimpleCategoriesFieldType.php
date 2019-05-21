221557aec9c1fac71c6dc09ae0aa61706d05a222
Tim Kelty <tim@fusionary.com>
2019-05-21 10:16:28 -0400

    Always select the deepest, for single selection types that are nested.

--- fieldtypes/SimpleCategories_SimpleCategoriesFieldType.php
+++ fieldtypes/SimpleCategories_SimpleCategoriesFieldType.php
@@ -46,7 +46,7 @@ class SimpleCategories_SimpleCategoriesFieldType extends CategoriesFieldType
 		}
 
         $vars = $this->getInputTemplateVariables($name, $criteria);
-        $vars['values'] = $criteria ? $criteria->ids() : [];
+        $vars['values'] = $criteria ? $criteria->order('lft desc')->ids() : [];
         $groupCriteria = craft()->elements->getCriteria(ElementType::Category);
         $groupCriteria->limit = null;
         $groupCriteria->groupId = $source['criteria']['groupId'];
